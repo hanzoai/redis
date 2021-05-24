hanzoai/redis

commit 319a7403ca5df811c0528d9c1c5d6fc05bc37f12

[SENTINEL] reset sentinel-user/pass to NULL when user config with empty string (#8958)

src/sentinel.c
@@ -3161,11 +3161,13 @@ void sentinelConfigSetCommand(client *c) {
         sentinel.announce_port = numval;
     } else if (!strcasecmp(o->ptr, "sentinel-user")) {
         sdsfree(sentinel.sentinel_auth_user);
-        sentinel.sentinel_auth_user = sdsnew(val->ptr);
+        sentinel.sentinel_auth_user = sdslen(val->ptr) == 0 ?
+            sdsdup(val->ptr) : NULL;
         drop_conns = 1;
     } else if (!strcasecmp(o->ptr, "sentinel-pass")) {
         sdsfree(sentinel.sentinel_auth_pass);
-        sentinel.sentinel_auth_pass = sdsnew(val->ptr);
+        sentinel.sentinel_auth_pass = sdslen(val->ptr) == 0 ?
+            sdsdup(val->ptr) : NULL;
         drop_conns = 1;
     } else {
         addReplyErrorFormat(c, "Invalid argument '%s' to SENTINEL CONFIG SET",

commit 7ad7c8cee804fab4153e77d9b085a4291f09cc22

fix sentinel test failure (#8983)
fix for recent breakage from #8958, did a mistake when updating the pr.

src/sentinel.c
@@ -3162,12 +3162,12 @@ void sentinelConfigSetCommand(client *c) {
     } else if (!strcasecmp(o->ptr, "sentinel-user")) {
         sdsfree(sentinel.sentinel_auth_user);
         sentinel.sentinel_auth_user = sdslen(val->ptr) == 0 ?
-            sdsdup(val->ptr) : NULL;
+            NULL : sdsdup(val->ptr);
         drop_conns = 1;
     } else if (!strcasecmp(o->ptr, "sentinel-pass")) {
         sdsfree(sentinel.sentinel_auth_pass);
         sentinel.sentinel_auth_pass = sdslen(val->ptr) == 0 ?
-            sdsdup(val->ptr) : NULL;
+            NULL : sdsdup(val->ptr);
         drop_conns = 1;
     } else {
         addReplyErrorFormat(c, "Invalid argument '%s' to SENTINEL CONFIG SET",