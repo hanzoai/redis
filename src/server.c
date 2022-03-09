hanzoai/redis

commit 214fad59e1d37b50ca5b813940d5f754e574c6fd

dismiss COW of client output buffer now that it's dynamic (#10371)
since #9822, the static reply buffer is no longer part of the client structure, so we need to dismiss it.

src/server.c
@@ -6417,7 +6417,8 @@ void dismissMemory(void* ptr, size_t size_hint) {
 
 /* Dismiss big chunks of memory inside a client structure, see dismissMemory() */
 void dismissClientMemory(client *c) {
-    /* Dismiss client query buffer. */
+    /* Dismiss client query buffer and static reply buffer. */
+    dismissSds(c->buf);
     dismissSds(c->querybuf);
     dismissSds(c->pending_querybuf);
     /* Dismiss argv array only if we estimate it contains a big buffer. */
@@ -6443,9 +6444,6 @@ void dismissClientMemory(client *c) {
             if (bulk) dismissMemory(bulk, bulk->size);
         }
     }
-
-    /* The client struct has a big static reply buffer in it. */
-    dismissMemory(c, 0);
 }
 
 /* In the child process, we don't need some buffers anymore, and these are

commit 4a23fa07e02673b79d8331c0674eb97212ae8554

Use dismissMemory to dismiss COW of client output buffer (#10403)
c->buf is not sds, so we should use dismissMemory instead of dismissSds to dismiss it.
This is a recent regression from #10371

src/server.c
@@ -6418,7 +6418,7 @@ void dismissMemory(void* ptr, size_t size_hint) {
 /* Dismiss big chunks of memory inside a client structure, see dismissMemory() */
 void dismissClientMemory(client *c) {
     /* Dismiss client query buffer and static reply buffer. */
-    dismissSds(c->buf);
+    dismissMemory(c->buf, c->buf_usable_size);
     dismissSds(c->querybuf);
     dismissSds(c->pending_querybuf);
     /* Dismiss argv array only if we estimate it contains a big buffer. */